ef0e25f70e257460a4ff762fbea6b591f12e48b4
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-04-04 00:40:56 -0300

    feat: req 2 and 3 concluded - add error middlewares

--- middlewares/checkNameAndQuantity.js
+++ middlewares/checkNameAndQuantity.js
@@ -1,11 +1,20 @@
 const ERR = require('../services/errorMessages');
 
-const checkNameAndQuantity = (req, _res, next) => {
-  const { name, quantity} = req.body;
+const checkName = (name) => {
   if (!name) throw ERR.NAME_IS_REQUIRED;
-  if (!quantity) throw ERR.QUANTITY_IS_REQUIRED;
   if (name.length < 5) throw ERR.NAME_LENGTH;
-  if (quantity < 0 && typeof quantity === 'number') throw ERR.QUANTITY_MUST_BE_GREATER_THAN_0;
+};
+
+const checkQuantity = (quantity) => {
+  if (quantity === undefined) throw ERR.QUANTITY_IS_REQUIRED;
+  if (quantity <= 0 && typeof quantity === 'number') throw ERR.QUANTITY_MUST_BE_GREATER_THAN_0;
+};
+
+const checkNameAndQuantity = (req, _res, next) => {
+  const { name = '', quantity } = req.body;
+  checkName(name);
+  checkQuantity(quantity);
   next();
-}
+};
+
 module.exports = checkNameAndQuantity;
--- middlewares/checkProductIdAndQuantity.js
+++ middlewares/checkProductIdAndQuantity.js
@@ -1,11 +1,20 @@
 const ERR = require('../services/errorMessages');
 
-const checkProductIdAndQuantity = (req, _res, next) => {
-  const { productId, quantity } = req.body;
-  if (quantity === undefined && productId) throw ERR.QUANTITY_IS_REQUIRED;
-  if (!productId && quantity) throw ERR.PRODUCT_ID_IS_REQUIRED;
-  if (quantity === undefined && !productId) throw ERR.QUANTITY_IS_REQUIRED;
+const checkProductId = (productId) => {
+  if (!productId) throw ERR.PRODUCT_ID_IS_REQUIRED;
+};
+
+const checkQuantity = (quantity) => {
+  if (!quantity) throw ERR.QUANTITY_IS_REQUIRED;
   if (quantity < 0 || typeof quantity === 'number') throw ERR.QUANTITY_MUST_BE_GREATER_THAN_0;
+};
+
+const checkProductIdAndQuantity = (req, _res, next) => {
+  req.body.forEach((element) => {
+    const { productId, quantity } = element;
+    checkProductId(productId);
+    checkQuantity(quantity);
+  });
   next();
 };
 
